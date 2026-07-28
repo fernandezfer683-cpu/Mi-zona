caf881a831b13a61206b5b34e6650efa6f135542
fernandezfer683-cpu <fernandezfer683@gmail.com>
2026-07-28 18:28:43 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "TU_API_KEY",
   authDomain: "mi-zona-webbb.firebaseapp.com",
   projectId: "mi-zona-webbb",
-  storageBucket: "mi-zona-webbb.firebasestorage.app",
   messagingSenderId: "118606465797",
   appId: "1:118606465797:web:bae262464d9a627505c56c"
 };
@@ -14,4 +12,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-export const storage = getStorage(app);
