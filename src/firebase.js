d043cc00544e1cbeb78b43b1dc1e74d8e0be0ac6
fernandezfer683-cpu <fernandezfer683@gmail.com>
2026-07-28 12:24:54 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Pegá acá el bloque de configuración que te dio Firebase
-// (Configuración del proyecto > Tus apps > Config)
 const firebaseConfig = {
-  apiKey: "AIzaSyCNimj076ZzH1UgaJiL8mxQqRZBIOfZwTM",
-  authDomain: "mi-zona-4e6ef.firebaseapp.com",
-  projectId: "mi-zona-4e6ef",
-  storageBucket: "mi-zona-4e6ef.firebasestorage.app",
-  messagingSenderId: "875768628292",
-  appId: "1:875768628292:web:4de259a72d90cdad506811",
+  apiKey: "TU_API_KEY",
+  authDomain: "mi-zona-webbb.firebaseapp.com",
+  projectId: "mi-zona-webbb",
+  storageBucket: "mi-zona-webbb.firebasestorage.app",
+  messagingSenderId: "118606465797",
+  appId: "1:118606465797:web:bae262464d9a627505c56c"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
 export const storage = getStorage(app);
